2cb19d65baaf3535ee132b229e8812738f8dff2b
sirazievalisa <sirazievalisa@gmail.com>
2026-03-01 21:04:02 +0300

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -30,3 +30,52 @@ FROM (
 ) t
 ORDER BY day_number, seller;
 
+-- Таблица с разбивкой на возрастные группы покупателей
+SELECT
+    CASE
+        WHEN age BETWEEN 16 AND 25 THEN '16-25'
+        WHEN age BETWEEN 26 AND 40 THEN '26-40'
+        WHEN age BETWEEN 42 AND 55 THEN '5-15'
+        ELSE '40+'
+    END AS age_category,
+    COUNT(*) AS age_count
+FROM customers
+GROUP BY age_category
+ORDER BY age_category;
+
+
+-- Таблица с количеством покупателей и выручкой по месяцам
+SELECT
+  TO_CHAR(s.sale_date, 'YYYY-MM') AS selling_month,
+  COUNT(DISTINCT s.customer_id) AS total_customers,
+  FLOOR(SUM(s.quantity * p.price)) AS income
+FROM sales s
+JOIN products p ON p.product_id = s.product_id
+GROUP BY selling_month
+ORDER BY selling_month;
+
+
+-- Таблица с покупателями первая покупка которых пришлась на время проведения специальных акций
+WITH promo_sales AS (
+    SELECT
+        s.customer_id,
+        s.sale_date,
+        s.sales_person_id,
+        s.product_id,
+        ROW_NUMBER() OVER (
+            PARTITION BY s.customer_id
+            ORDER BY s.sale_date, s.sales_id
+        ) AS rn
+    FROM sales s
+)
+SELECT
+    c.first_name || ' ' || c.last_name AS customer,
+    ps.sale_date,
+    e.first_name || ' ' || e.last_name AS seller
+FROM promo_sales ps
+JOIN products p   ON p.product_id = ps.product_id
+JOIN employees e  ON e.employee_id = ps.sales_person_id
+JOIN customers c  ON c.customer_id = ps.customer_id
+WHERE ps.rn = 1
+  AND p.price = 0
+ORDER BY c.customer_id;
