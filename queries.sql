64d04870b9a27bed884a82641a6d3728a33ba243
sirazievalisa <sirazievalisa@gmail.com>
2026-03-01 19:04:52 +0300

    Add files via upload

---
+++ queries.sql
@@ -0,0 +1,10 @@
+SELECT 
+count (*) AS customers_count
+from customers;
+
+/*
+Данный запрос:
+1. Обращается к таблице customers
+2. Считает количество строк
+3. Возвращает результат в колонке customers_count
+*/
