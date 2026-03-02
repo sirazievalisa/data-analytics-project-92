82f8d6c716925e2d44fd05fd59d94b39e56acca9
sirazievalisa <sirazievalisa@gmail.com>
2026-03-02 20:12:19 +0300

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -35,8 +35,7 @@ SELECT
     CASE
         WHEN age BETWEEN 16 AND 25 THEN '16-25'
         WHEN age BETWEEN 26 AND 40 THEN '26-40'
-        WHEN age BETWEEN 42 AND 55 THEN '5-15'
-        ELSE '40+'
+                ELSE '40+'
     END AS age_category,
     COUNT(*) AS age_count
 FROM customers
