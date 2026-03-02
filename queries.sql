916aced011bb49aa9974ed62694b3fa5928d0724
sirazievalisa <sirazievalisa@gmail.com>
2026-03-02 20:44:02 +0300

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -5,30 +5,37 @@
 3. Возвращает результат в колонке customers_count
 */
 
-SELECT 
-count (*) AS customers_count
-from customers;
+SELECT count(*) AS customers_count
+FROM customers;
 
 
 -- Отчет 3: с данными по выручке по каждому продавцу и дню недели
 SELECT
-    seller,
-    day_of_week,
-    income
+    t.seller,
+    t.day_of_week,
+    t.income
 FROM (
     SELECT
-        CONCAT(e.first_name, ' ', e.last_name) AS seller,
-        LOWER(TRIM(TO_CHAR(s.sale_date, 'FMDay'))) AS day_of_week,
-        EXTRACT(ISODOW FROM s.sale_date) AS day_number,
-        FLOOR(SUM(p.price * s.quantity)) AS income
-    FROM sales s
-    JOIN employees e
-        ON e.employee_id = s.sales_person_id
-    JOIN products p
-        ON p.product_id = s.product_id
-    GROUP BY seller, day_of_week, day_number
-) t
-ORDER BY day_number, seller;
+        concat(e.first_name, ' ', e.last_name) AS seller,
+        lower(
+            trim(to_char(s.sale_date, 'FMDay'))
+        ) AS day_of_week,
+        extract(ISODOW FROM s.sale_date) AS day_number,
+        floor(sum(p.price * s.quantity)) AS income
+    FROM sales AS s
+    INNER JOIN employees AS e
+        ON s.sales_person_id = e.employee_id
+    INNER JOIN products AS p
+        ON s.product_id = p.product_id
+    GROUP BY
+        seller,
+        day_of_week,
+        day_number
+) AS t
+ORDER BY
+    t.day_number,
+    t.seller;
+
 
 -- Таблица с разбивкой на возрастные группы покупателей
 SELECT
@@ -37,7 +44,7 @@ SELECT
         WHEN age BETWEEN 26 AND 40 THEN '26-40'
         ELSE '40+'
     END AS age_category,
-    COUNT(*) AS age_count
+    count(*) AS age_count
 FROM customers
 GROUP BY age_category
 ORDER BY age_category;
@@ -45,36 +52,44 @@ ORDER BY age_category;
 
 -- Таблица с количеством покупателей и выручкой по месяцам
 SELECT
-  TO_CHAR(s.sale_date, 'YYYY-MM') AS selling_month,
-  COUNT(DISTINCT s.customer_id) AS total_customers,
-  FLOOR(SUM(s.quantity * p.price)) AS income
-FROM sales s
-JOIN products p ON p.product_id = s.product_id
+    to_char(s.sale_date, 'YYYY-MM') AS selling_month,
+    count(DISTINCT s.customer_id) AS total_customers,
+    floor(sum(s.quantity * p.price)) AS income
+FROM sales AS s
+INNER JOIN products AS p
+    ON s.product_id = p.product_id
 GROUP BY selling_month
 ORDER BY selling_month;
 
 
--- Таблица с покупателями первая покупка которых пришлась на время проведения специальных акций
+-- Таблица с покупателями, 1я покупка которых пришлась на время проведения акций
 WITH promo_sales AS (
     SELECT
         s.customer_id,
         s.sale_date,
         s.sales_person_id,
         s.product_id,
-        ROW_NUMBER() OVER (
+        row_number() OVER (
             PARTITION BY s.customer_id
-            ORDER BY s.sale_date, s.sales_id
+            ORDER BY
+                s.sale_date,
+                s.sales_id
         ) AS rn
-    FROM sales s
+    FROM sales AS s
 )
+
 SELECT
-    c.first_name || ' ' || c.last_name AS customer,
     ps.sale_date,
+    c.first_name || ' ' || c.last_name AS customer,
     e.first_name || ' ' || e.last_name AS seller
-FROM promo_sales ps
-JOIN products p   ON p.product_id = ps.product_id
-JOIN employees e  ON e.employee_id = ps.sales_person_id
-JOIN customers c  ON c.customer_id = ps.customer_id
-WHERE ps.rn = 1
-  AND p.price = 0
+FROM promo_sales AS ps
+INNER JOIN products AS p
+    ON ps.product_id = p.product_id
+INNER JOIN employees AS e
+    ON ps.sales_person_id = e.employee_id
+INNER JOIN customers AS c
+    ON ps.customer_id = c.customer_id
+WHERE
+    ps.rn = 1
+    AND p.price = 0
 ORDER BY c.customer_id;
